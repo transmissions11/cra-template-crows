d95709fc55e19cf37d11a0c1f46a6a7a8fbaf889
Transmissions <greenfeatherhelp@gmail.com>
2020-04-08 09:16:39 -0700

    Add  "react-ios-pwa-prompt": "^1.7.0" and chakra-ui

--- template/src/index.js
+++ template/src/index.js
@@ -5,7 +5,26 @@ import App from "./components/App";
 
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./index.css";
+
+import { ThemeProvider, CSSReset, theme } from "@chakra-ui/core";
+
+ReactDOM.render(
+  <>
+    <PWAPrompt
+      timesToShow={10}
+      permanentlyHideOnDismiss={false}
+      copyTitle="Add YOUR_APP to Home Screen"
+      copyBody="YOUR_APP works best when added to your homescreen. Without doing this, you may have a degraded experience."
+      copyClosePrompt="Close"
+    />
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <App />
+    </ThemeProvider>
+  </>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
